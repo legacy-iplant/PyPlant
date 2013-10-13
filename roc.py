87ebebc0bab424233dea218780fcc71adaf12978
Dustin Landers <dustin.landers@gmail.com>
2013-10-13 13:32:12 -0400

    using roc to build roc curves

--- roc.py
+++ roc.py
@@ -1,25 +1,8 @@
 ## Just a script for messing with roc in matplotlib
-"""
-execfile('pyplant.py')
+
+from pyplant import *
+
+usr = 'dalanders'
+psw = 'Shadow@3876'
 
 token = GetToken(usr,psw)
-sim_original = DownloadFile(usr,token,'analyses/plink-sim10-out/simulation1_--assoc.qassoc.adjusted')
-
-sim = Data(sim_original)
-sim.ChangeToFloat(tuple(range(2,10)))
-sim.ChangeToInt(0)
-
-assoc = Data(DownloadFile(usr,token,'analyses/plink-sim10-out/simulation1_--assoc.qassoc'))
-assoc.ChangeToFloat(tuple(range(2,10)))
-assoc.ChangeToInt(0)
-"""
-def FindLow(object, point, p=8, marker=1):
-	my_markers = list()
-	num = 0
-	for row in range(object.nrow):
-		if object.data[row][8] < point:
-			num += 1
-			my_markers.append(object.data[row][marker])
-	print num
-	return my_markers
-
