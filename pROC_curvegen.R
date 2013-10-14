56f18a8724a430b11c41102af39864524cc00ae7
Dustin Landers <dustin.landers@gmail.com>
2013-10-14 15:31:22 -0400

    minor changes

--- pROC_curvegen.R
+++ pROC_curvegen.R
@@ -1,9 +1,11 @@
 require(pROC)
 
-sim10 <- read.csv(file='test.txt',header=T)
-known_truths <- c('csu303', 'gpm551', 'AY112215', 'IDP2516', 'umc1803', 'umc1174', 'gpm249b', 'php15024')
-known_effects <- c(2,2,3,3,5,5,7,7)
+sim10 <- read.csv(file='sim10.txt',header=T)
 
-sim10$truth <- ifelse(sim10$SNP %in% known_truths, 'Pos', 'Neg')
+known_truths_sim10 <- c('csu303', 'gpm551', 'AY112215', 'IDP2516', 'umc1803', 'umc1174', 'gpm249b', 'php15024')
+known_effects_sim10 <- c(2,2,3,3,5,5,7,7)
 
-plot.roc(sim10$truth,sim10$P,main='Simmons-Simulation10',print.thres='best',percent=T,print.auc=T,col='#1c61b6')
+sim10$truth <- ifelse(sim10$SNP %in% known_truths_sim10, 'Pos', 'Neg')
+
+rocobj1 <- plot.roc(sim10$truth,sim10$P,main="PLINK ROC Curve Using Simmon's Simulation 10 Dataset",
+	print.thres='best',percent=T,print.auc=T,col='#1c61b6')
